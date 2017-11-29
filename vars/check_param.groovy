f0bdc994340980ecc23ce803d76030739715579b
Dmitry Sergeev <identw@gmail.com>
2017-11-29 20:33:39 +0500

    Test

--- vars/check_param.groovy
+++ vars/check_param.groovy
@@ -1,10 +1,12 @@
 def call (param) {
-    try {
-        println Eval.me(param)
-    } catch(e) {
-        echo "Not set variable ${param}"
-        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
-        currentBuild.description = "Not set variable ${param}"
-        currentBuild.result = 'FAILURE'
-    }
+    println Eval.me(param)
 }
+//    try {
+//        println Eval.me(param)
+//    } catch(e) {
+//        echo "Not set variable ${param}"
+//        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
+//        currentBuild.description = "Not set variable ${param}"
+//        currentBuild.result = 'FAILURE'
+//    }
+//}
