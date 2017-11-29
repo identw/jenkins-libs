ebc58b9250053a298fe360c39b011b7ca3b3ee64
Dmitry Sergeev <identw@gmail.com>
2017-11-29 21:18:28 +0500

    Fix

--- vars/check_param.groovy
+++ vars/check_param.groovy
@@ -1,8 +1,10 @@
 def call (param) {
-    if ( ! binding.hasVariable(param) ) {
-        echo "Not set variable ${param}"
-        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
-        currentBuild.description = "Not set variable ${param}"
-        currentBuild.result = 'FAILURE'
-    }
+//    if ( ! binding.hasVariable(param) ) {
+//        echo "Not set variable ${param}"
+//        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
+//        currentBuild.description = "Not set variable ${param}"
+//        currentBuild.result = 'FAILURE'
+//    }
+
+    binding.variables.each {k,v -> println "$k = $v"}
 }
