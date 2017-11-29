8d1cfeb0f6ca0f942f5de0e8025bc73b24b8e90b
Dmitry Sergeev <identw@gmail.com>
2017-11-29 21:05:10 +0500

    check_param

--- vars/check_param.groovy
+++ vars/check_param.groovy
@@ -1,13 +1,8 @@
 def call (param) {
-    web_root = "222"
-    println Eval.me(param)
+    if ( ! binding.hasVariable(param) ) {
+        echo "Not set variable ${param}"
+        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
+        currentBuild.description = "Not set variable ${param}"
+        currentBuild.result = 'FAILURE'
+    }
 }
-//    try {
-//        println Eval.me(param)
-//    } catch(e) {
-//        echo "Not set variable ${param}"
-//        currentBuild.displayName = "#${BUILD_NUMBER} Not set variable ${param}"
-//        currentBuild.description = "Not set variable ${param}"
-//        currentBuild.result = 'FAILURE'
-//    }
-//}
