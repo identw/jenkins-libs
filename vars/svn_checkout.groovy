dddb07c23e3d75376577571bfe53a54b170057dc
Dmitry Sergeev <identw@gmail.com>
2017-12-01 01:22:00 +0500

    added svn_checkout

---
+++ vars/svn_checkout.groovy
@@ -0,0 +1,3 @@
+def call(svn, local = './', credentialsId) {
+    checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: credentialsId, depthOption: 'infinity', ignoreExternalsOption: true, local: local, remote: svn]], workspaceUpdater: [$class: 'UpdateWithCleanUpdater']])
+}
