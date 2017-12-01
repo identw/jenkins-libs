1bdc983d436b394d98f404057afa98d1170ca3cc
Dmitry Sergeev <identw@gmail.com>
2017-12-01 21:31:35 +0500

    fix

--- vars/not_remove_dirs.groovy
+++ vars/not_remove_dirs.groovy
@@ -1,6 +1,6 @@
 def call(dirs) {
-    def not_remove_dirs = '('
-    for (i = 0; i < dirs.size(); i++) {
+    def not_remove_dirs = '(' + dirs[0]
+    for (i = 1; i < dirs.size(); i++) {
         not_remove_dirs = not_remove_dirs + '|' + dirs[i]
     }
     not_remove_dirs = not_remove_dirs + ')'
