2868d440853ad07530fd28cc924130e70302f37c
Dmitry Sergeev <identw@gmail.com>
2017-12-12 22:13:16 +0500

    Fix

--- vars/get_revisions.groovy
+++ vars/get_revisions.groovy
@@ -3,7 +3,7 @@ def call(dirs) {
     def revs
     for (i = 0; i < dirs.size(); i++) {
         dir(dirs[i]) {
-            revs.put( dirs[i],sh_return('svn info --show-item last-changed-revision') )
+            revs.put( dirs[i], sh returnStdout: true, script: 'svn info --show-item last-changed-revision' )
         }
     }
     return revs
