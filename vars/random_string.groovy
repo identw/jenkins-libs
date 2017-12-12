6438d87490ac9748a20118c61fd6a91b16f62689
Dmitry Sergeev <identw@gmail.com>
2017-12-12 12:28:34 +0500

    Fix

--- vars/random_string.groovy
+++ vars/random_string.groovy
@@ -1,4 +1,4 @@
-def call() {
-    def tmp = sh returnStdout: true, script: 'echo -n "`cat /dev/urandom | tr -dc \'a-zA-Z0-9\' | fold -w 32 | head -n 1`"'
+def call(len = 12) {
+    def tmp = sh returnStdout: true, script: "echo -n \"`cat /dev/urandom | tr -dc \'a-zA-Z0-9\' | fold -w ${len} | head -n 1`\""
     return tmp
 }
