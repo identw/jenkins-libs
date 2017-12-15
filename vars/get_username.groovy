21c1adce6e100f976ddd920ee6e1d0bbd17b7ffe
Dmitry Sergeev <identw@gmail.com>
2017-12-15 18:04:44 +0500

    get_username - added RemoteCause

--- vars/get_username.groovy
+++ vars/get_username.groovy
@@ -1,6 +1,17 @@
 def call() {
     def build = currentBuild.rawBuild
     def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-    def name = cause.getUserName()
+    def name
+    if ( cause != null ) {
+        name = cause.getUserName()
+    } else {
+        cause = build.getCause(hudson.model.Cause.RemoteCause.class)
+        if ( cause != null ) {
+            name = cause.getShortDescription()
+        } else {
+            name = "UNKNOWN"
+        }
+    }
+
     return name
 }
