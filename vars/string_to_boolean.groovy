a8bd58bcc9cf1c341bcc75366dce35e332e6a585
Dmitry Sergeev <identw@gmail.com>
2018-04-03 14:38:38 +0500

    Fix

--- vars/string_to_boolean.groovy
+++ vars/string_to_boolean.groovy
@@ -1,5 +1,10 @@
 def call(string) {
     def bool = false
+
+    if (string.getClass() == Boolean) {
+        return string
+    }
+
     switch (string) {
         case 'false':
             bool = false
