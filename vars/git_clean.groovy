ad75a6d85b966a0c759138364aaa15c932338525
Dmitry Sergeev <identw@gmail.com>
2019-05-24 12:28:08 +0500

    Add git prune fir git_clean

--- vars/git_clean.groovy
+++ vars/git_clean.groovy
@@ -2,7 +2,9 @@ def call(all = false) {
     if (all) {
         sh 'git clean -X -f -f -d'
         sh 'git clean -f -f -d && git checkout .'
+        sh 'git prune'
     } else {
         sh 'git clean -f -d && git checkout .'
+        sh 'git prune'
     }
 }
