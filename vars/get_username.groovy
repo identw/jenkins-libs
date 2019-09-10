fa95ca493b00117c7e930bbc8fcfd6a794ff567d
Dmitry Sergeev <identw@gmail.com>
2019-09-10 11:52:11 +0500

    get cause support upstream

--- vars/get_username.groovy
+++ vars/get_username.groovy
@@ -1,17 +1,20 @@
 def call() {
     def build = currentBuild.rawBuild
     def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-    def name
+
     if ( cause ) {
-        name = cause.getUserName()
-    } else {
-        cause = build.getCause(hudson.model.Cause.RemoteCause.class)
-        if ( cause ) {
-            name = cause.getShortDescription()
-        } else {
-            name = "UNKNOWN"
-        }
+        return cause.getUserName()
     }
 
-    return name
+    cause = build.getCause(hudson.model.Cause.RemoteCause.class)
+    if ( cause ) {
+        return cause.getShortDescription()
+    } 
+    
+    cause = build.getCause(hudson.model.Cause.UpstreamCause.class) 
+    if ( cause ) {
+       return cause.getShortDescription() 
+    }
+
+    return 'UNKNOWN'
 }
