5f7bdeefcca686a10336e057453cb90fe18ec2a2
Dmitry Sergeev <identw@gmail.com>
2019-09-17 12:44:57 +0500

    Added the ability to pull the username  who started the job from another job

--- vars/get_username.groovy
+++ vars/get_username.groovy
@@ -1,19 +1,29 @@
 def call() {
     def build = currentBuild.rawBuild
     def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-
-    if ( cause ) {
-        return cause.getUserName()
-    }
-
-    cause = build.getCause(hudson.model.Cause.RemoteCause.class)
-    if ( cause ) {
-        return cause.getShortDescription()
-    } 
     
-    cause = build.getCause(hudson.model.Cause.UpstreamCause.class) 
-    if ( cause ) {
-       return cause.getShortDescription() 
+    def i = 0;
+    while (i < 3) {
+        if ( cause ) {
+            return cause.getUserName()
+        }
+
+        cause = build.getCause(hudson.model.Cause.RemoteCause.class)
+        if ( cause ) {
+            return cause.getShortDescription()
+        } 
+
+        cause = build.getCause(hudson.model.Cause.UpstreamCause.class) 
+        if ( cause ) {
+            def job = Jenkins.instance.getItemByFullName(cause.getUpstreamProject())
+            def buildId = cause.getUpstreamBuild() 
+            build = job.getBuildByNumber(buildId)
+
+            cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+            continue
+        }
+
+        i++
     }
 
     return 'UNKNOWN'
