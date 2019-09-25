0737801a756a18423ccc6624cef62fc9f096c232
Dmitry Sergeev <identw@gmail.com>
2019-09-25 11:46:24 +0500

    normilize branch

--- vars/normalize_branch.groovy
+++ vars/normalize_branch.groovy
@@ -1,3 +1,6 @@
 def call(branch) {
+    if ( branch.contains('origin') ) {
+        branch = branch.split('/').drop(1).join('/')
+    }
     return branch.toLowerCase().replaceAll("[^a-z0-9]", '-')
 }
