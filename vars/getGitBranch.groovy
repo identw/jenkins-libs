83e6ef62d197b5648689f0c1e7eef983ff2dd45c
Dmitry Sergeev <identw@gmail.com>
2019-11-19 17:13:49 +0500

    fix getGitBranch

--- vars/getGitBranch.groovy
+++ vars/getGitBranch.groovy
@@ -1,3 +1,10 @@
 def call(ref) {
-    return sh (returnStdout: true, script: "git branch -a --contains ${ref} | tail -n1 | sed 's@.*origin/\\(.*\\)\$@\\1@g'").trim()
+    def commit = sh (returnStdout: true, script: "git rev-parse ${ref}").trim()
+    def headBranch = sh(returnStdout: true, script: "find .git/refs/remotes -type f | while read f; do if cat \${f} | grep '${commit}' -q; then echo \${f};fi;done | sed 's@^\\./@@;s@.*origin/\\(.*\\)\$@\\1@g'").trim()
+
+    if (headBranch == '') {
+        return sh (returnStdout: true, script: "git branch -a --contains ${ref} | tail -n1 | sed 's@.*origin/\\(.*\\)\$@\\1@g'").trim()
+    } else {
+        return headBranch
+    }
 }
